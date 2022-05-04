f0ae5452b3e01640efd20848e25f82f34a25683d
marko <91672368+primatm@users.noreply.github.com>
2022-05-04 21:21:38 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const accordions = document.querySelectorAll(".accordion");
+document.querySelector('.btn').onclick = function (e) {
+    let menu = document.querySelector('.menu');
+    let btn = document.querySelector('.btn');
 
+    menu.classList.toggle('is-active');
+    btn.classList.toggle('is-active');
+
+    e.preventDefault();
+}
+
+const accordions = document.querySelectorAll(".accordion");
 accordions.forEach(accordion => {
     accordion.addEventListener("click", () => {
         accordion.classList.toggle("active");
